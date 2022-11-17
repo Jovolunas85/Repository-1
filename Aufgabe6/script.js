63381a385382cd987376662c5380fbba3c1c9fe1
Medin <115719571+Jovolunas85@users.noreply.github.com>
2022-11-17 16:50:12 +0100

    ah

---
+++ Aufgabe6/script.js
@@ -0,0 +1,12 @@
+//Einwohnerzahlen der Laender (in Mio)
+const gerpop = 83.13;
+const frapop = 67.50;
+const itapop = 59.07;
+const swepop = 10.42;
+//Intenetnutzer der Laender (in Mio)
+const gerusers = 74.65;
+const frausers = 57.24;
+const itausers = 41.64;
+const sweusers = 9.85;
+console.log();
+//# sourceMappingURL=script.js.map
