c0a3f8dd0d6c3441fdf0053b59843d55f26f0a31
Medin <115719571+Jovolunas85@users.noreply.github.com>
2022-11-17 19:11:35 +0100

    ag

--- Aufgabe6/script.js
+++ Aufgabe6/script.js
@@ -1,12 +1,16 @@
-//Einwohnerzahlen der Laender (in Mio)
-const gerpop = 83.13;
-const frapop = 67.50;
-const itapop = 59.07;
-const swepop = 10.42;
-//Intenetnutzer der Laender (in Mio)
-const gerusers = 74.65;
-const frausers = 57.24;
-const itausers = 41.64;
-const sweusers = 9.85;
-console.log();
+//Einwohnerzahlen der Laender 2022 (in Mio)
+const germany = 83.13;
+const france = 67.50;
+const italy = 59.07;
+const sweden = 10.42;
+const europeanunion = 447.70;
+//Einwohnerzahlen der Laender 2008 (in Mio)
+const deutschland = 82.11;
+const frankreich = 64.37;
+const italien = 58.83;
+const schweden = 9.22;
+console.log("Die Einwohnerzahl Deutschlands betrug im Jahr 2022: " + germany + " Mio. Das beträgt " + germany / europeanunion * 100 + "% der gesamten Einwohnerzahl der EU. Die Wachstumsrate seit 2008 betraegt: ");
+console.log("Die Einwohnerzahl Frankreichs betrug im Jahr 2022: " + france + " Mio. Das beträgt " + france / europeanunion * 100 + "% der gesamten Einwohnerzahl der EU.");
+console.log("Die Einwohnerzahl Italiens betrug im Jahr 2022: " + italy + " Mio. Das beträgt " + italy / europeanunion * 100 + "% der gesamten Einwohnerzahl der EU.");
+console.log("Die Einwohnerzahl Schwedens betrug im Jahr 2022: " + sweden + " Mio. Das beträgt " + sweden / europeanunion * 100 + "% der gesamten Einwohnerzahl der EU.");
 //# sourceMappingURL=script.js.map
