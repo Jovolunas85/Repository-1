999bdd829e22d60ee17eb4bdeae5ed8a8b692695
Medin <115719571+Jovolunas85@users.noreply.github.com>
2022-12-18 16:02:20 +0100

    hgb

--- Aufgabe10/script.js
+++ Aufgabe10/script.js
@@ -1,11 +1,29 @@
 var A10;
 (function (A10) {
+    let checkbox = document.getElementById('checkbox');
+    let meineListe = document.getElementsByTagName("LI");
+    let j;
+    for (j = 0; j < meineListe.length; j++) {
+        var schliessen = document.createElement("SPAN");
+        var text = document.createTextNode("\u00D7");
+        schliessen.className = "close";
+        schliessen.appendChild(text);
+        meineListe[j].appendChild(schliessen);
+    }
+    let close = document.getElementsByClassName("close");
+    let k;
+    for (k = 0; k < close.length; k++) {
+        close[k].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none";
+        };
+    }
     document.querySelector("#liste").addEventListener('keydown', function (e) {
         if (e.key == 'Enter') {
-            const wrapper = document.querySelector("#tasks");
-            const newDiv = document.createElement("div");
-            newDiv.classList.add("p");
-            wrapper.appendChild(newDiv);
+            //const wrapper = document.querySelector("#tasks")
+            //const newDiv = document.createElement("div");
+            //newDiv.classList.add("p");
+            //wrapper.appendChild(newDiv);
         }
     });
 })(A10 || (A10 = {}));
--- Aufgabe10/script.ts
+++ Aufgabe10/script.ts
@@ -1,11 +1,35 @@
 namespace A10 {
+    let checkbox = document.getElementById('checkbox',) as HTMLInputElement | null;
+    let meineListe = document.getElementsByTagName("LI");
+    let j;
+    for(j = 0; j < meineListe.length; j++){
+        var schliessen = document.createElement("SPAN");
+        var text = document.createTextNode("\u00D7");
+        schliessen.className = "close";
+        schliessen.appendChild(text);
+        meineListe[j].appendChild(schliessen);
+    }
+    let close = document.getElementsByClassName("close");
+    let k;
+    for(k = 0; k < close.length; k++){
+        close[k].onclick = function(){
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+    
+    
+
 
 document.querySelector("#liste").addEventListener('keydown',function(e:KeyboardEvent) {
     if(e.key=='Enter'){
-        const wrapper = document.querySelector("#tasks")
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("p");
-        wrapper.appendChild(newDiv);
+        //const wrapper = document.querySelector("#tasks")
+        //const newDiv = document.createElement("div");
+        //newDiv.classList.add("p");
+        //wrapper.appendChild(newDiv);
     }
 })
+
+
+
 }
