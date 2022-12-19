7a7381644e005b64db92b3d17cb2822eadbc147f
Medin <115719571+Jovolunas85@users.noreply.github.com>
2022-12-19 18:28:34 +0100

    ff

--- Aufgabe10/script.js
+++ Aufgabe10/script.js
@@ -1,30 +1,61 @@
 var A10;
 (function (A10) {
-    let checkbox = document.getElementById('checkbox');
-    let meineListe = document.getElementsByTagName("LI");
-    let j;
-    for (j = 0; j < meineListe.length; j++) {
-        var schliessen = document.createElement("SPAN");
-        var text = document.createTextNode("\u00D7");
-        schliessen.className = "close";
-        schliessen.appendChild(text);
-        meineListe[j].appendChild(schliessen);
-    }
-    let close = document.getElementsByClassName("close");
-    let k;
-    for (k = 0; k < close.length; k++) {
-        close[k].onclick = function () {
-            let div = this.parentElement;
-            div.style.display = "none";
-        };
-    }
-    document.querySelector("#liste").addEventListener('keydown', function (e) {
-        if (e.key == 'Enter') {
-            //const wrapper = document.querySelector("#tasks")
-            //const newDiv = document.createElement("div");
-            //newDiv.classList.add("p");
-            //wrapper.appendChild(newDiv);
-        }
+    let number = document.getElementById("zahl");
+    let counter = 0;
+    window.addEventListener('load', () => {
+        const form = document.querySelector("#new-task");
+        const input = document.querySelector("#input");
+        const list_el = document.querySelector("#tasks");
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            counter++;
+            console.log(counter);
+            number.innerHTML = "" + counter;
+            const task = input.value;
+            if (!task) {
+                alert("Please add a task");
+                return;
+            }
+            const checkmark = document.createElement("div");
+            const check = document.createElement("input");
+            check.setAttribute("type", "checkbox");
+            check.classList.add("check");
+            checkmark.appendChild(check);
+            const task_el = document.createElement("div");
+            task_el.classList.add("task");
+            const task_content_el = document.createElement("div");
+            task_content_el.classList.add("content");
+            task_el.appendChild(checkmark);
+            task_el.appendChild(task_content_el);
+            const task_input_el = document.createElement("input");
+            task_input_el.classList.add("text");
+            task_input_el.type = "text";
+            task_input_el.value = task;
+            task_input_el.setAttribute("readonly", "readonly");
+            task_content_el.appendChild(task_input_el);
+            const task_actions_el = document.createElement("div");
+            task_actions_el.classList.add("actions");
+            const task_delete_el = document.createElement("button");
+            task_delete_el.classList.add("delete");
+            task_delete_el.innerHTML = "Delete";
+            task_el.appendChild(task_actions_el);
+            task_actions_el.appendChild(task_delete_el);
+            list_el.appendChild(task_el);
+            input.value = "";
+            task_delete_el.addEventListener('click', () => {
+                list_el.removeChild(task_el);
+                counter--;
+                number.innerHTML = "" + counter;
+            });
+        });
     });
+    //document.querySelector("#liste").addEventListener('keydown',function(e:KeyboardEvent) {
+    //    if(e.key=='Enter'){
+    //        const wrapper = document.querySelector("#tasks")
+    //        const newDiv = document.createElement("li");
+    //        newDiv.classList.add("li");
+    //        wrapper.appendChild(newDiv);
+    //    }
+    //})
 })(A10 || (A10 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe10/script.ts
+++ Aufgabe10/script.ts
@@ -1,12 +1,67 @@
 namespace A10 {
     
     let number: HTMLHeadingElement = <HTMLHeadingElement>document.getElementById("zahl");
-
-    document.querySelector("#liste").addEventListener('keydown',function(e:KeyboardEvent) {
+    let counter = 0;
+    window.addEventListener ('load',() =>{
+        const form = document.querySelector("#new-task");
+        const input = document.querySelector("#input") as HTMLInputElement;
+        const list_el = document.querySelector("#tasks");
         
-    })
+        form.addEventListener('submit',(e)=>{
+            e.preventDefault();
 
+            counter++;
+            console.log(counter);
+            number.innerHTML= ""+counter;
 
+            const task = input.value;
+
+            if(!task){alert("Please add a task");
+            return;
+            }
+
+            
+            const checkmark: HTMLDivElement = document.createElement("div");
+            const check: HTMLInputElement = document.createElement("input");
+            check.setAttribute("type","checkbox")
+            check.classList.add("check");
+            checkmark.appendChild(check);
+
+            const task_el = document.createElement("div");
+            task_el.classList.add("task");
+            const task_content_el = document.createElement("div");
+            task_content_el.classList.add("content");
+            task_el.appendChild(checkmark);
+            task_el.appendChild(task_content_el);
+            
+            const task_input_el = document.createElement("input");
+            task_input_el.classList.add("text");
+            task_input_el.type = "text";
+            task_input_el.value = task;
+            task_input_el.setAttribute("readonly","readonly");
+            task_content_el.appendChild(task_input_el);
+
+            const task_actions_el = document.createElement("div");
+            task_actions_el.classList.add("actions");
+            
+            const task_delete_el = document.createElement("button");
+            task_delete_el.classList.add("delete");
+            task_delete_el.innerHTML = "Delete";
+            task_el.appendChild(task_actions_el);
+            task_actions_el.appendChild(task_delete_el);
+            list_el.appendChild(task_el);
+
+            input.value = ""
+
+            task_delete_el.addEventListener('click',()=>{
+                list_el.removeChild(task_el);
+                counter--;
+                number.innerHTML=""+counter;
+            }
+            )
+
+        }
+        )})
 //document.querySelector("#liste").addEventListener('keydown',function(e:KeyboardEvent) {
 //    if(e.key=='Enter'){
 //        const wrapper = document.querySelector("#tasks")
@@ -15,7 +70,5 @@ namespace A10 {
 //        wrapper.appendChild(newDiv);
 //    }
 //})
-
-
 
 }
